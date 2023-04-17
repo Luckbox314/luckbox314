2272399f8e5ec060aa42a35132b3f6a28dcecb47
Luckbox314 <lucas.muoz@uc.cl>
2023-04-17 01:34:51 -0400

    App created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article id='article'>
+      <div>
+        <h1 className='luckbox'>Luckbox</h1>
+      </div>
+      <div className='content'>
+        <div className='card'>
+          <h2>Protitypes</h2>
+          <p>ideas you can try out.</p>
+        </div>
+        <div className='card'>
+          <h2>Ideas</h2>
+          <p>List of ideas that I have.</p>
+        </div>
+        <div className='card'>
+          <h2>React Components</h2>
+          <p>List of react components that I've created and I find kind of neat.</p>
+        </div>
+        <div className='card'>
+          <h2>Curriculum</h2>
+          <p>List of my work experience</p>
+        </div>
+      </div>
+      </article>
+      <footer>
+        Social media
+      </footer>
     </div>
   );
 }
