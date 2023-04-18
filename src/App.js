b10c60e84d69925c0b3ca7fdf404261a51f246a1
Luckbox314 <lucas.muoz@uc.cl>
2023-04-18 10:47:44 -0400

    clickable links

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ function App() {
         <h1 className='luckbox'>Luckbox</h1>
       </div>
       <div className='content'>
-        <div className='card'>
-          <h2>Prototypes</h2>
-          <p>ideas you can try out.</p>
-        </div>
-        <div className='card'>
-          <h2>Ideas</h2>
-          <p>List of ideas that I have.</p>
-        </div>
-        <div className='card'>
-          <h2>React Components</h2>
-          <p>List of react components that I've created and I find kind of neat.</p>
-        </div>
-        <div className='card'>
-          <h2>Curriculum</h2>
-          <p>List of my work experience</p>
+        <a className="clickCard" target='_blank' href="https://luckbox.notion.site/General-Art-Projects-86d26e45510843eebaa04ab00cd7cd18">
+          <div className='card'>
+            <h2>General art Ideas</h2>
+            <p>Ideas/projects I'll some day get arount to finish.</p>
+          </div>
+        </a>
+        <a className="clickCard" target='_blank' href="https://luckbox.notion.site/Game-Ideas-c83359c307df4db08f80ed97b1f1ce34">
+          <div className='card'>
+            <h2>Game Ideas</h2>
+            <p>Same as general art ideas but for games.</p>
+          </div>
+        </a>
+        <a className="clickCard" target='_blank' href="https://www.overleaf.com/download/project/62db2c91127a7dc14ea52850/build/18794d1cf92-9675fa5bae851046/output/output.pdf?compileGroup=standard&clsiserverid=clsi-pre-emp-e2-c-f-cnsm&enable_pdf_caching=true">
+          <div className='card'>
+            <h2>Curriculum</h2>
+            <p>List of my work experience</p>
+          </div>
+        </a>
+        <div className='card invisible'>
+          Filler
         </div>
       </div>
       </article>
